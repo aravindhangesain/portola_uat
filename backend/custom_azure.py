0aa59132f40ff677e862f93b9acc2fa5e7ac0e5f
Aravindhan <aravindhan@gesain.net>
2025-11-06 16:18:26 +0530

    azure key error fixed

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -13,13 +13,13 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'portalstore1'  # Must be replaced when going to PROD
-    account_key = 'WZCC8HIgX6V1MnhYNqrVhb/V0DM9G7g+AyvS59tdKbSX5sZbV7I0wkiP14vbyHqniJe1P/pYbT0+AStytkQGw=='
+    account_key = '/WZCC8HIgX6V1MnhYNqrVhb/V0DM9G7g+AyvS59tdKbSX5sZbV7I0wkiP14vbyHqniJe1P/pYbT0+AStytkQGw=='
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'portalstore1'  # Must be replaced when going to PROD
-    account_key = 'WZCC8HIgX6V1MnhYNqrVhb/V0DM9G7g+AyvS59tdKbSX5sZbV7I0wkiP14vbyHqniJe1P/pYbT0+AStytkQGw=='
+    account_key = '/WZCC8HIgX6V1MnhYNqrVhb/V0DM9G7g+AyvS59tdKbSX5sZbV7I0wkiP14vbyHqniJe1P/pYbT0+AStytkQGw=='
     azure_container = 'static'
     expiration_secs = None
 
